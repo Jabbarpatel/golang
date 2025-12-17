c0a2cdb0888e61cb3cdae67f48a586bb5cfc7fe6
JabbarPatel <jabbarpatel1947@gmail.com>
2025-12-17 17:25:19 +0000

    added drawer component

---
+++ development/frontend/src/components/General/ReactDrawer.tsx
@@ -0,0 +1,41 @@
+import React, { JSX, ReactNode } from "react";
+import { Drawer, Divider } from "rsuite";
+
+export type Props = {
+  open: boolean;
+  children: ReactNode;
+  handleClose?: () => void;
+  size?: string;
+  title?: string;
+  footer?: () => JSX.Element | null;
+};
+
+const ReactDrawer = ({
+  open,
+  children,
+  handleClose = () => {},
+  size = "xs",
+  title = "This is title",
+  footer = () => <span>This is footer</span>,
+}: Props) => {
+  return (
+    <Drawer open={open} onClose={handleClose} size={size}>
+      <Drawer.Header>
+        <Drawer.Title style={{ fontWeight: "bold" }}>{title}</Drawer.Title>
+      </Drawer.Header>
+      <Drawer.Body
+        style={{
+          padding: 10,
+          height: "74%",
+          scrollbarWidth: "thin",
+        }}
+      >
+        {children}
+      </Drawer.Body>
+      <Divider />
+      <Drawer.Actions style={{ padding: 5 }}>{footer()}</Drawer.Actions>
+    </Drawer>
+  );
+};
+
+export default ReactDrawer;
