4c2f2cabb8e9be1c1c09353d6e37567c8fa472ad
Jabbar <j.patel@bm-systems.in>
2026-01-06 14:00:45 +0200

    removed unused code

--- development/frontend/src/components/Routes/Admin/Users/Users.tsx
+++ development/frontend/src/components/Routes/Admin/Users/Users.tsx
@@ -1,31 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import { TableCell, TableRow } from "@heroui/react";
-// import * as userActions from "@/actions/users_api";
-// import AppTable from "@/components/General/AppTable";
-
 const Users = () => {
-  // const [users, setUsers] = useState([]);
-  // const [page, setPage] = useState<number>(1);
-  // const [pageSize, setPageSize] = useState<number>(10);
-  // const [totalPages, setTotalPages] = useState<number>();
-
-  // const {
-  //   data: usersData,
-  //   status: isUsers,
-  //   refetch: refetchUsers,
-  // } = userActions.useGetUsers(page, pageSize);
-
-  // useEffect(() => {
-  //   refetchUsers();
-  // }, []);
-
-  // useEffect(() => {
-  //   if (isUsers === "success") {
-  //     setUsers(usersData.Data);
-  //     setTotalPages(usersData.TotalPages);
-  //   }
-  // }, [isUsers]);
-
   return <>This users tab</>;
 };
 export default Users;
